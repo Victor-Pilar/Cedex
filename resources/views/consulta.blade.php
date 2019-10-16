03050378fc291b79d4436d8e835f56f38f398f60
VizardSkor <vizardscorpion@gmail.com>
2019-10-15 21:27:48 -0500

    Añadi un Collapse
    quite el modal y puse un modal, un no se me ocurre como mejorar la parte del diagnostico.

--- resources/views/consulta.blade.php
+++ resources/views/consulta.blade.php
@@ -5,34 +5,25 @@
   @csrf
   <div class="row">
     <div class="col-8">
-    <div class="form-group col-md-4">
+    <div class="form-group col-md-8">
       <label for="text">Bascar Paciente</label>
       <input type="text" class="form-control" name="BPaciente" placeholder="Buscar Paciente">
+      <br>
       <button type="submit" class="btn btn-primary">Buscar</button>
-      <button type="submit" class="btn btn-info" data-toggle="modal" data-target="#myModal">Nuevo Paciente</button>
-      <!--Modal-->
-      <div class="container">
-        <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          
-          <h4 class="modal-title">Nuevo Paciente</h4>
-        </div>
-        <div class="modal-body">
-    <!--Datos Principales -->
+
+
+  <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Nuevo Paciente</button>
+  <div id="demo" class="collapse">
   <div class="form-row">
-    <div class="form-group col-md-4">
+    <div class="form-group col-md-8">
       <label for="text">Nombre</label>
       <input type="text" class="form-control" name="nombre" placeholder="Nombre (s)">
     </div>
-    <div class="form-group col-md-4">
+    <div class="form-group col-md-8">
       <label for="inputappat">Apellido Paterno</label>
       <input type="text" class="form-control" name="appat" placeholder="Apellido Paterno">
     </div>
-      <div class="form-group col-md-4">
+      <div class="form-group col-md-8">
       <label for="inputapmat">Apellido Materno</label>
       <input type="text" class="form-control" name="apmat" placeholder="Apellido Materno">
     </div>
@@ -40,39 +31,32 @@
 
   <!--Datos del Domicilio -->
   <div class="form-row">
-    <div class="form-group col-md-4">
+    <div class="form-group col-md-8">
       <label>Domicilio</label>
       <input type="text" class="form-control" name="calle" placeholder="Calle">
     </div>
-     <div class="form-group col-md-4">
+     <div class="form-group col-md-8">
       <label for="inputCol">Colonia</label>
       <input type="text" class="form-control" name="colonia" placeholder="Colonia">
     </div>
-    <div class="form-group col-md-4">
+    <div class="form-group col-md-8">
       <label for="inputMunicipio">Municipio</label>
       <input type="text" class="form-control" name="mun" placeholder="Municipio">
     </div>
   </div>
-        </div>
-          <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
   </div>
-</div>
+
 
     </div>
  <!--Datos Médicos Básicos -->
   <div class="form-row">
     <div class="form-group col-md-2">
-      <label for="inputtalla">Talla</label>
-      <input type="text" class="form-control" name="talla">
+      <label for="inputtalla">Estatura</label>
+      <input type="text" class="form-control" name="Estatura">
     </div>
     <div class="form-group col-md-2">
       <label for="inputtalla">Peso</label>
-      <input type="text" class="form-control" name="talla">
+      <input type="text" class="form-control" name="Peso">
     </div>
     <div class="form-group col-md-2">
       <label for="inputSex">Sexo</label>
@@ -86,10 +70,14 @@
       <input type="text" class="form-control" name="edad">
     </div>
   </div>
-  <label for="receta">Diagnostico</label>
+  <label for="receta"><h3>Diagnostico</h3></label>
+      <div class="form-group col-md-2">
+      <label for="inputtalla">Sintomas</label>
+      <input type="text" class="form-control" name="talla">
+    </div>
     <textarea class="form-control" id="receta" rows="3" name="diagnost"></textarea>
   
-  <button type="submit" class="btn btn-primary">Siguiente</button>
+  <button type="submit" class="btn btn-primary">Continuar</button>
 </form>
     </div>
   </div>
