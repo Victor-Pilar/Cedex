e85dabd5076f3eef6b971a806833ad3525a13016
VizardSkor <vizardscorpion@gmail.com>
2019-10-15 19:14:54 -0500

    Prueba_2
    Solo estoy cambiando la bienvenida, para hacer pruebas.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CEDEX Bienveni3 2.0
+                    CEDEX Bienveni3 3.0
                 </div>
 
                 <div class="links">
